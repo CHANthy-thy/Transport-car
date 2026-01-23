88b2c01a338ee45a30a5abe9996007b80c0678f0
SREYVIK.VON <SREYVIK.VON@PNC.LOCAL>
2026-01-23 09:56:01 +0700

    Add user gmial

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 const users = [
-    { id: 1, email: 'tola@gmail.com', password: '12345', role: 'admin' },
+    { id: 1, email: 'sokom@gmail.com', password: '12345', role: 'admin' },
     { id: 2, email: 'gta@gmail.com', password: '12345', role: 'user' },
+    { id: 3, email: 'tola@gmail.com', password: '12345', role: 'user' },
+    { id: 4, email: 'sreyvik@gmail.com', password: '12345', role: 'user' },
+    { id: 5, email: 'reourn@gmail.com', password: '12345', role: 'user' },
+    { id: 6, email: 'chanthy@gmail.com', password: '12345', role: 'user' },
 ];
 // isLogin
 if(localStorage.getItem('role')){
